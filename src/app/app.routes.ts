c6d36eaa7d71ee0e9013864369515e67edfdee1c
Kai Schulz <info@sparrowmedia.de>
2025-01-08 14:55:14 +0100

    added single user component + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SingleUserComponent } from './main-page/single-user/single-user.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'single-user', component: SingleUserComponent },
+];
---
+++ src/app/main-page/single-user/single-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-user.component.html',
+  styleUrl: './single-user.component.scss'
+})
+export class SingleUserComponent {
+
+}
