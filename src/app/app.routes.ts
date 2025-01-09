61847b3ab3350caae42e3800dc4172c12a33d52b
Kai Schulz <info@sparrowmedia.de>
2025-01-09 11:33:11 +0100

    single user styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoginComponent } from './login-page/login/login.component';
 import { SingleUserComponent } from './main-page/single-user/single-user.component';
+import { ImprintComponent } from './legal/imprint/imprint.component';
+import { DataProtectionComponent } from './legal/data-protection/data-protection.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: '', component: MainPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'impressum', component: ImprintComponent},
+  { path: 'datenschutzerklaerung', component: DataProtectionComponent},
   { path: 'single-user', component: SingleUserComponent },
 ];
