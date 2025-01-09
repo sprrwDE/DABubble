f6e3275ba1b0cab9cbaaa0911a032a1031e88a4b
Trane <Trane_67@hotmail.de>
2025-01-09 12:44:30 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { LoginComponent } from './login-page/login/login.component';
 import { SingleUserComponent } from './main-page/single-user/single-user.component';
 import { ImprintComponent } from './legal/imprint/imprint.component';
 import { DataProtectionComponent } from './legal/data-protection/data-protection.component';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginPageComponent },
   { path: 'impressum', component: ImprintComponent},
   { path: 'datenschutzerklaerung', component: DataProtectionComponent},
   { path: 'single-user', component: SingleUserComponent },
