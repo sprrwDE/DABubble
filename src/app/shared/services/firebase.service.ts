f9e4fbbe86813c3c47a65dbe32614ebe8d6c2204
Kai Schulz <info@sparrowmedia.de>
2025-01-10 11:29:50 +0100

    added firebase logic to get user

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, onSnapshot, DocumentData } from '@angular/fire/firestore';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
+  private fetchedCollectionSubject = new BehaviorSubject<DocumentData[]>([]);
+  fetchedCollection$ = this.fetchedCollectionSubject.asObservable();
 
-  constructor() { }
+  constructor(private firestore: Firestore) {}
+
+  getData(db: string) {
+    try {
+      const colRef = collection(this.firestore, db);
+
+      onSnapshot(colRef, (snapshot) => {
+        const fetchedData: DocumentData[] = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        this.fetchedCollectionSubject.next(fetchedData);
+
+        console.log('Daten abgerufen:', fetchedData);
+      });
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Daten:', error);
+    }
+  }
 }
