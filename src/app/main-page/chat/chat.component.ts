ea0af4b09c4a8d08cefc74a0af189410a7ecab22
Mr.X <aq-computer@outlook.com>
2025-01-10 10:03:22 +0800

    add scrollbar style + main chat input field

--- src/app/main-page/chat/chat.component.ts
+++ src/app/main-page/chat/chat.component.ts
@@ -1,12 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
-export class ChatComponent {
-
-}
+export class ChatComponent {}
