6072e49fcbe3f8e81863731c5ef4e4a7648d1142
Mr.X <aq-computer@outlook.com>
2025-01-11 12:01:46 +0800

    added inputs for user-message content

--- src/app/main-page/chat/user-message/user-message.component.ts
+++ src/app/main-page/chat/user-message/user-message.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-message',
   standalone: true,
   imports: [],
   templateUrl: './user-message.component.html',
-  styleUrl: './user-message.component.scss'
+  styleUrl: './user-message.component.scss',
 })
 export class UserMessageComponent {
-
+  @Input() message: string = '';
+  @Input() time: string = '';
+  @Input() name: string = '';
+  @Input() imgUrl: string = '';
 }
