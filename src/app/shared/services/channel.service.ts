91bbef5ba4f15184c982b8b7d23767bde4d6045e
Kai Schulz <info@sparrowmedia.de>
2025-01-16 12:03:44 +0100

    moved channel logic to channel service

--- src/app/shared/services/channel.service.ts
+++ src/app/shared/services/channel.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@angular/core';
+import { FirebaseService } from '../../shared/services/firebase.service';
 import { Channel } from '../models/channel.model';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { NgZone } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChannelService {
-  constructor(public channel: Channel) { 
+  private currentChannelData$ = new BehaviorSubject<Channel | null>(null);
+  private unsubscribe?: () => void;
+
+  constructor(private fb: FirebaseService, private ngZone: NgZone) {}
+
+  fetchChannel(channelId: string): void {
+    if (this.unsubscribe) {
+      this.unsubscribe(); 
+    }
+
+    this.unsubscribe = this.fb.subscribeToSingleDoc('channels', channelId, (data) => {
+      this.ngZone.run(() => {
+        this.currentChannelData$.next(new Channel(data));
+      });
+    });
   }
 
+  getChannel(): Observable<Channel | null> {
+    return this.currentChannelData$.asObservable();
+  }
+
+  unsubscribeChannel(): void {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+      this.unsubscribe = undefined;
+    }
+  }
 }
