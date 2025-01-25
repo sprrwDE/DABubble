dc32709197cc7b1c42851843313680b66f4d8dfb
Trane <Trane_67@hotmail.de>
2025-01-25 19:32:25 +0100

    login responsiv

--- src/app/login-page/animation-screen/animation-screen.component.ts
+++ src/app/login-page/animation-screen/animation-screen.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { GlobalVariablesService } from '../../shared/services/global-variables.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-animation-screen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './animation-screen.component.html',
   styleUrl: './animation-screen.component.scss',
 })
