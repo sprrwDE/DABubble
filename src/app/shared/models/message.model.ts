bddbf526750716311bc8bea80edf97f1b8ca7f38
Kai Schulz <info@sparrowmedia.de>
2025-02-07 18:40:37 +0100

    rendering works now as model was changed thx chat

--- src/app/shared/models/message.model.ts
+++ src/app/shared/models/message.model.ts
@@ -19,14 +19,20 @@ export class Message {
     this.message = obj?.message || '';
     this.userId = obj?.userId || '';
     // this.likes = obj?.likes || 0;
-    this.likes = obj?.reactions
-      ? obj.reactions.map((reaction: any) => ({
-          emoji: reaction.emoji,
-          count: reaction.count,
-          userIds: reaction.userIds || [], 
-        }))
-      : [];
-    this.timestamp = obj?.timestamp || null;
+    this.likes = obj?.likes // Falls likes direkt existiert, benutze es
+    ? obj.likes.map((like: any) => ({
+        emoji: like.emoji,
+        count: like.count,
+        userIds: like.userIds || [],
+      }))
+    : obj?.reactions // Falls reactions existiert, nutze es stattdessen
+    ? obj.reactions.map((reaction: any) => ({
+        emoji: reaction.emoji,
+        count: reaction.count,
+        userIds: reaction.userIds || [],
+      }))
+    : [];
+      this.timestamp = obj?.timestamp || null;
     this.replies = obj?.replies
       ? obj.replies.map((rep: any) => new Reply(rep))
       : [];
