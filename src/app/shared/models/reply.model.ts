3e40feba461a52cb7957bfb0957e1c54df92329e
Mr.X <aq-computer@outlook.com>
2025-02-15 18:38:22 +0800

    fixed bug

--- src/app/shared/models/reply.model.ts
+++ src/app/shared/models/reply.model.ts
@@ -9,26 +9,26 @@ export class Reply {
   userId: string;
   timestamp: number;
   id: string;
-    likes: Reaction[];
+  likes: Reaction[];
 
   constructor(obj?: any) {
     this.message = obj?.message || '';
     this.userId = obj?.userId || '';
     this.timestamp = obj?.timestamp || null;
-    this.id = obj?.id || ''
-    this.likes = obj?.likes 
-    ? obj.likes.map((like: any) => ({
-        emoji: like.emoji,
-        count: like.count,
-        userIds: like.userIds || [],
-      }))
-    : obj?.reactions 
-    ? obj.reactions.map((reaction: any) => ({
-        emoji: reaction.emoji,
-        count: reaction.count,
-        userIds: reaction.userIds || [],
-      }))
-    : [];
+    this.id = obj?.id || '';
+    this.likes = obj?.likes
+      ? obj.likes.map((like: any) => ({
+          emoji: like.emoji,
+          count: like.count,
+          userIds: like.userIds || [],
+        }))
+      : obj?.reactions
+      ? obj.reactions.map((reaction: any) => ({
+          emoji: reaction.emoji,
+          count: reaction.count,
+          userIds: reaction.userIds || [],
+        }))
+      : [];
   }
 
   toJSON() {
