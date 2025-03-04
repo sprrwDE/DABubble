4eeb8d987564689843711cb2d72cdd9473f83166
Kai Schulz <info@sparrowmedia.de>
2025-03-04 18:31:29 +0100

    added some imprint and data protection styling

--- src/app/legal/imprint/imprint.component.ts
+++ src/app/legal/imprint/imprint.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { GlobalVariablesService } from '../../shared/services/global-variables.service';
 
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+  constructor(public global: GlobalVariablesService) {}
 
 }
